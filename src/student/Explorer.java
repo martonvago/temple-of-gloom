ad335e7c4ed94d6c63dbfc3990202603e469ca05
Marton Vago <marton.vago@softwire.com>
2022-11-13 23:04:54 +0000

    add basic explore and escape implementations

--- src/student/Explorer.java
+++ src/student/Explorer.java
@@ -2,8 +2,17 @@ package student;
 
 import game.EscapeState;
 import game.ExplorationState;
+import game.Node;
+import game.NodeStatus;
+
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Explorer {
+    private List<Node> bestEscapePath = new ArrayList<>();
 
     /**
      * Explore the cavern, trying to find the orb in as few steps as possible.
@@ -36,7 +45,22 @@ public class Explorer {
      * @param state the information available at the current state
      */
     public void explore(ExplorationState state) {
-        //TODO : Explore the cavern and find the orb
+        Stack<Long> moves = new Stack<>();
+        List<Long> deadEnds = new ArrayList<>();
+
+        while (state.getDistanceToTarget() != 0) {
+            moves.push(state.getCurrentLocation());
+            Optional<NodeStatus> unexploredNeighbourClosestToOrb = state.getNeighbours()
+                    .stream()
+                    .filter(nodeStatus -> !moves.contains(nodeStatus.nodeID()) && !deadEnds.contains(nodeStatus.nodeID()))
+                    .min(Comparator.comparingInt(NodeStatus::distanceToTarget));
+            if (unexploredNeighbourClosestToOrb.isEmpty()) {
+                deadEnds.add(moves.pop());
+                state.moveTo(moves.pop());
+                continue;
+            }
+            state.moveTo(unexploredNeighbourClosestToOrb.get().nodeID());
+        }
     }
 
     /**
@@ -63,6 +87,87 @@ public class Explorer {
      * @param state the information available at the current state
      */
     public void escape(EscapeState state) {
-        //TODO: Escape from the cavern before time runs out
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+        Runnable findBestPath = () -> step(
+                state.getCurrentNode(),
+                new ArrayList<>(),
+                state.getExit(),
+                state.getTimeRemaining()
+        );
+        Future<?> futureFindBestPath = executor.submit(findBestPath);
+        Runnable cancelSearch = () -> futureFindBestPath.cancel(true);
+        executor.schedule(cancelSearch, 10, TimeUnit.SECONDS);
+        executor.shutdown();
+
+        try {
+            futureFindBestPath.get();
+        } catch (Exception ignored) {
+            // Search time limit exceeded
+        }
+
+        pickUpGoldIfAny(state);
+        bestEscapePath.stream()
+                .skip(1)
+                .forEach(node -> {
+                    state.moveTo(node);
+                    pickUpGoldIfAny(state);
+                });
+    }
+
+    private void pickUpGoldIfAny(EscapeState state) {
+        if (state.getCurrentNode().getTile().getGold() > 0) {
+            state.pickUpGold();
+        }
+    }
+
+    private void step(Node next, List<Node> moves, Node exit, int maxWeight) {
+        if (Thread.currentThread().isInterrupted()) {
+            return;
+        }
+
+        Node prev = moves.size() > 0 ? moves.get(moves.size() - 1) : null;
+        moves.add(next);
+
+        if (getTotalWeightForNodes(moves) > maxWeight) {
+            return;
+        }
+
+        if (next.equals(exit)) {
+            if (isNewPathBetter(moves, bestEscapePath)) {
+                bestEscapePath = moves;
+                return;
+            }
+        }
+
+        next.getNeighbours().stream()
+                .filter(neighbour -> !neighbour.equals(prev))
+                .forEach(neighbour -> step(neighbour, new ArrayList<>(moves), exit, maxWeight));
+    }
+
+    private boolean isNewPathBetter(List<Node> newPath, List<Node> bestPath) {
+        if (bestPath.isEmpty()) {
+            return true;
+        }
+
+        if (getAllGoldForNodes(newPath) > getAllGoldForNodes(bestPath)) {
+            return true;
+        }
+        if (getAllGoldForNodes(newPath) < getAllGoldForNodes(bestPath)) {
+            return false;
+        }
+        return getTotalWeightForNodes(newPath) < getTotalWeightForNodes(bestPath);
+    }
+
+    private int getTotalWeightForNodes(List<Node> path) {
+        int sum = 0;
+        for (int i = 1; i < path.size(); i++) {
+            Node prev = path.get(i - 1);
+            sum += path.get(i).getEdge(prev).length();
+        }
+        return sum;
+    }
+
+    private int getAllGoldForNodes(List<Node> path) {
+        return path.stream().mapToInt(node -> node.getTile().getGold()).sum();
     }
 }
